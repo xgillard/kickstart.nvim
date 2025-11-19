b42e81f6f4ec27577005816ad74e94c26a53fed7
Xavier GILLARD <xaviergillard@gmail.com>
2025-11-19 17:43:35 +0100

    remap les touches [ et ] vers < et > pour la navigation sur un clavier azerty

--- init.lua
+++ init.lua
@@ -183,6 +183,10 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Remap des touches navigationnelles du clavier US
+vim.keymap.set('n', '<', '[', { remap = true, desc = 'Agit comme [ pour la navigation' })
+vim.keymap.set('n', '>', ']', { remap = true, desc = 'Agit comme ] pour la navigation' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
